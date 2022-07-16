88b27c7a6d5cadef0411011fbb05e3f5ae494e94
rubenverkuylen <info@rubenverkuylen.nl>
2022-07-16 14:16:55 +0200

    Safari update, scrollbar updates
    Still have to figure out scrollbar issues for mobile

---
+++ todo.js
@@ -0,0 +1,19 @@
+// TODO:
+// x clicked artist laten highlighten
+// x playe slide in
+// x svg images as background
+// x include bandcamp iframe player
+// x active link ook links
+// x styling <a> met juiste kleuren.
+// x Mobile en groot scherm
+
+//
+// FIXME:
+// JS
+// x refactoring expand functions
+// - bypass als soundcloud API niet werkt > in functie plaatsen waarbinnen het nodig is. > refactoring
+// x make SVG grow, not jump
+// x play track when loaded > load event finish?
+// x safari flexbox geeft problemen
+// x stop track playing when DISC expands
+// - reload BC ifrrame als je op SC klikt
